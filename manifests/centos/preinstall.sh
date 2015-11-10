c79e48b45e33dbeed3f82178bea658a2abe13e33
James Peach <jorgar@gmail.com>
2015-11-10 14:30:03 -0800

    Update Fedora and Centos definitions.
        - Dump Fedora 20 and Fedora 21.
        - Update the EPEL package for CentOS 7.
        - Refresh ISO URLs and switch to sha256 checksums.

--- manifests/centos/preinstall.sh
+++ manifests/centos/preinstall.sh
@@ -4,7 +4,7 @@ echo Installing YUM repositories for puppet and EPEL ..
 case $(cat /etc/redhat-release) in
     *7.[0-9]*)
         rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
-        rpm -ivh http://download.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-2.noarch.rpm
+        rpm -ivh http://download.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
         ;;
     *6.[0-9]*)
         rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rp
