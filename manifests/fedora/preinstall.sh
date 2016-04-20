cf60f07fd6ac8e186a46c04481470a74d4428ab1
James Peach <jpeach@apache.org>
2016-04-20 11:42:27 -0700

    Fix the Fedora 23 vagrant box.
    The Fedora 23 box should not get EPEL installed, because EPEL is
    only for CentOS and RHEL. We also need to update the version of
    puppet that is installed and add compatibility symlinks for the
    packer puppet provisioner.

--- manifests/fedora/preinstall.sh
+++ manifests/fedora/preinstall.sh
@@ -1,1 +1,66 @@
-../centos/preinstall.sh
+#! /bin/bash
+
+. /etc/os-release
+
+echo Installing YUM repository for puppet ...
+case $VERSION_ID in
+    23)
+    cat > /etc/yum.repos.d/puppet.repo <<EOF
+[puppetlabs]
+name = Puppet Labs
+baseurl = https://yum.puppetlabs.com/fedora/f23/PC1/\$basearch
+gpgkey = https://yum.puppetlabs.com/RPM-GPG-KEY-puppetlabs
+enabled = 1
+gpgcheck = 1
+EOF
+    ;;
+    22)
+    rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-fedora-22.noarch.rpm
+    ;;
+esac
+
+echo Installing puppet ...
+dnf install -y puppet-agent
+
+echo Symlinking puppet binaries into /usr/sbin ...
+for prog in /opt/puppetlabs/bin/* ; do
+    ln -sf $prog /usr/sbin/$(basename $prog)
+done
+
+echo Installing well-known vagrant SSH key ...
+(
+    cd ~vagrant
+
+    mkdir -p .ssh
+    touch .ssh/authorized_keys
+
+    curl -o .ssh/authorized_keys -kL 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
+
+    chmod 700 .ssh
+    chmod 600 .ssh/authorized_keys
+    chown -R vagrant:vagrant .ssh
+)
+
+echo Installing VirtualBox guest additions ...
+(
+    cd ~vagrant
+    rm -f /var/log/vboxadd-install.log
+
+    if [ -r VBoxGuestAdditions.iso ] ; then
+        mount -o loop $(pwd)/VBoxGuestAdditions.iso /mnt
+
+        /mnt/VBoxLinuxAdditions.run --nox11
+
+        # If the build failed, puke some diagnostics ...
+        if [ "$?" -ne "0" ] ; then
+            cat /var/log/vboxadd-install.log
+            rpm -qa | grep kernel
+            echo kernel version is $(uname -r)
+            ls /lib/modules/$(uname -r)
+        fi
+
+        umount /mnt
+        rm VBoxGuestAdditions.iso
+    fi
+)
+
